ec847240b77fc6e7f029daf4d6f70a70389264f6
Christian Foster <fosterchris000@gmail.com>
2022-01-03 15:46:43 -0500

    Added global style updates when dragging items. This is accomplished by using onDragUpdate and onDragStart. In onDragEnd we reset the styles

--- src/index.js
+++ src/index.js
@@ -9,7 +9,22 @@ import "reset-css";
 const App = () => {
   const [appState, setAppState] = useState(initialData);
 
+  const onDragStart = () => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  const onDragUpdate = (update) => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(appState.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153,141,217, ${opacity})`;
+  };
+
   const onDragEnd = (result) => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -43,7 +58,11 @@ const App = () => {
   };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
       {appState.columnOrder.map((columnId) => {
         const column = appState.columns[columnId];
         const tasks = column.taskIds.map((taskId) => appState.tasks[taskId]);
