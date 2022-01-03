396ce52c291848612ef5b6d4a55c2b55b2bbba79
Christian Foster <fosterchris000@gmail.com>
2022-01-03 16:41:21 -0500

    Created a horizontal list example

--- src/Column.jsx
+++ src/Column.jsx
@@ -6,7 +6,6 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
-  width: 220px;
 
   display: flex;
   flex-direction: column;
@@ -19,15 +18,15 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
-  flex-grow: 1;
-  min-height: 100px;
+
+  display: flex;
 `;
 
 function Column(props) {
   return (
     <Container>
       <Title>{props.column.title}</Title>
-      <Droppable droppableId={props.column.id}>
+      <Droppable droppableId={props.column.id} direction="horizontal">
         {(provided, snapshot) => (
           <TaskList
             innerRef={provided.innerRef}
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,22 +1,32 @@
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
 const Container = styled.div`
-  border: 1px solid lightgrey;
-  border-radius: 2px;
+  border: 3px solid lightgrey;
+  border-radius: 50%;
   padding: 8px;
-  margin-bottom: 8px;
+  margin-right: 8px;
   background-color: ${(props) =>
     props.isDragDisabled
       ? "lightgrey"
       : props.isDragging
       ? "lightgreen"
       : "white"};
+  width: 40px;
+  height: 40px;
 
   display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus {
+    outline: none;
+    border-color: red;
+  }
 `;
 
 function Task(props) {
-  const isDragDisabled = props.task.id === "task-2";
+  let isDragDisabled = props.task.id === "task-2";
+  isDragDisabled = false;
   return (
     <Draggable
       draggableId={props.task.id}
@@ -32,7 +42,7 @@ function Task(props) {
           isDragging={snapshot.isDragging}
           isDragDisabled={isDragDisabled}
         >
-          {props.task.content}
+          {props.task.content[0]}
         </Container>
       )}
     </Draggable>
